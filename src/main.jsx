a863faf6cb30b934474d108d16c3d190a253ded5
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-08-29 15:01:48 -0300

    feat: config bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+import './styles/styles.scss'
+import * as bootstrap from 'bootstrap'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
