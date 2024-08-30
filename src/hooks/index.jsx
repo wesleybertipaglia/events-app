7d8abb6edb6e8c1da3a9510e387bd995c9554963
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-08-30 09:14:57 -0300

    style: remove useIsOwner

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,11 +1,9 @@
 import useApi from "./useApi"
 import useFetch from "./useFetch"
 import useAuth from "./useAuth"
-import useIsOwner from "./useIsOwner"
 
 export {
     useApi,
     useFetch,
-    useAuth,
-    useIsOwner
+    useAuth
 }
